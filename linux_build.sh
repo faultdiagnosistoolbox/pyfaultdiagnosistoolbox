f234626797412ac2ad5fee5ddd2c724b86cf91db
Erik Frisk <erik.frisk@liu.se>
2020-07-06 15:44:24 +0200

    Scripting a docker container to compile linux wheels

---
+++ linux_build.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# docker run -v `pwd`:/fdt_build buildpyfdt
+# docker run -it -v `pwd`:/fdt_build buildpyfdt /bin/bash
+
+cd src
+
+# Generate Python3.6
+. /py_env/env36/bin/activate
+make build_ext
+python setup.py bdist_wheel > log.txt
+wheel_name=`grep "creating 'dist/" log.txt | sed "s/.*dist\/\(.*\)' and .*/\1/g"`
+rm -f log.txt
+cd dist
+auditwheel repair $wheel_name
+rm -f $wheel_name
+mv -f wheelhouse/* .
+cd ..
+deactivate
+
+
+# Generate Python3.7
+. /py_env/env37/bin/activate
+python setup.py build_ext --inplace
+python setup.py bdist_wheel > log.txt
+wheel_name=`grep "creating 'dist/" log.txt | sed "s/.*dist\/\(.*\)' and .*/\1/g"`
+rm -f log.txt
+cd dist
+auditwheel repair $wheel_name
+rm -f $wheel_name
+mv -f wheelhouse/* .
+cd ..
+deactivate
+
+# Generate Python3.8
+. /py_env/env38/bin/activate
+python setup.py build_ext --inplace
+python setup.py bdist_wheel > log.txt
+wheel_name=`grep "creating 'dist/" log.txt | sed "s/.*dist\/\(.*\)' and .*/\1/g"`
+rm -f log.txt
+cd dist
+auditwheel repair $wheel_name
+rm -f $wheel_name
+mv -f wheelhouse/* .
+cd ..
+deactivate
+
+rm -rf src/dist/wheelhouse
+
