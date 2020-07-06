609247e21248f94db04ce2e31342c1680b4f20a5
Erik Frisk <erik.frisk@liu.se>
2020-07-06 17:17:40 +0200

    scripted macos build

---
+++ macos_build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cd src
+
+make cleanall
+# Generate Python3.8
+. ~/sw/pyenv/38/bin/activate
+make build_ext
+python setup.py bdist_wheel
+deactivate
+
+# Generate Python3.7
+. ~/sw/pyenv/37/bin/activate
+make build_ext
+python setup.py bdist_wheel
+deactivate
+
+
