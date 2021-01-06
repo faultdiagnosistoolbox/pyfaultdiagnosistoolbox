8084f9c6b9beb1a37f695e6416c4936b6ab98fd0
Erik Frisk <erik.frisk@liu.se>
2021-01-06 18:15:56 +0100

    Uppdaterat windows bygg-script

--- windows_build.bat
+++ windows_build.bat
@@ -2,18 +2,17 @@ call cd src
 
 nmake -f Makefile.win cleanall
 
-REM Generate Python3.6
-call ..\env36\Scripts\activate.bat
+call ..\..\env37\Scripts\activate
 call nmake -f Makefile.win build_ext
 call python setup.py bdist_wheel
 call deactivate
 
-call ..\env37\Scripts\activate
+call ..\..\env38\Scripts\activate
 call nmake -f Makefile.win build_ext
 call python setup.py bdist_wheel
 call deactivate
 
-call ..\env38\Scripts\activate
+call ..\..\env39\Scripts\activate
 call nmake -f Makefile.win build_ext
 call python setup.py bdist_wheel
 call deactivate
