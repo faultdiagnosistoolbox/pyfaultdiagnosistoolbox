9ee1c813b99df11250e80ed78648416d2c765cdb
Petrus Jarl <petja919@student.liu.se>
2026-03-06 12:28:49 +0000

    Setup pipeline

---
+++ format.sh
@@ -0,0 +1,7 @@
+black "$@" \
+src/faultdiagnosistoolbox/ExternalSimulation \
+src/faultdiagnosistoolbox/NeuralNetworkIntegration \
+src/faultdiagnosistoolbox/ModelTransforming \
+tests/test_external_simulation \
+tests/test_neural_network \
+tests/test_model_transforming
