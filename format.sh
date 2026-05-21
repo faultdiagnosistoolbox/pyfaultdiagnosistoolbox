4592d0f61dde3082a2f9ac83d04dc1099c543fc5
Sebastian Karlsson <sebka786@student.liu.se>
2026-05-21 06:39:42 +0200

    repo cleanup

--- format.sh
+++
@@ -1,7 +0,0 @@
-black "$@" \
-src/faultdiagnosistoolbox/ExternalSimulation \
-src/faultdiagnosistoolbox/NeuralNetworkIntegration \
-src/faultdiagnosistoolbox/ModelTransforming \
-tests/test_external_simulation \
-tests/test_neural_network \
-tests/test_model_transforming
